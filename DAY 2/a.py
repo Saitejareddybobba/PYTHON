7ef960360b73cdfaf4ed878caab7227da02d313f
Saitejareddybobba <86121208+Saitejareddybobba@users.noreply.github.com>
2021-07-01 00:01:28 +0530

    day 2
    some more problems done directly

---
+++ DAY 2/a.py
@@ -0,0 +1,5 @@
+def inPutS():
+    string = input()
+    s, i, f = string.split()
+    print(s + " " + str(int(i) + float(f)))
+    
---
+++ DAY 2/e.py
@@ -0,0 +1,8 @@
+# Function to perform increment and decrement
+def do_operation(X, Y):
+    # Your code here
+    X = X - 1
+    print (X)
+    
+    Y = Y + 1
+    print (Y)
